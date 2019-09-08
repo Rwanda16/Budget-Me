f599a5a5d03f031266acd9d5343bf4701b6d7b99
Rwanda16 <ghabiman@gmail.com>
2019-09-07 22:57:37 -0400

    formatted the budget using string manipulation.

--- app.js
+++ app.js
@@ -142,6 +142,19 @@ var UIController = (function() {
     container: ".container",
     expensesPercentageLabel: ".item__percentage"
   };
+  var formatNumber = function(num, type) {
+    var numSplit, num, int, dec, type;
+    num = Math.abs(num);
+    num = num.toFixed(2);
+
+    numSplit = num.split(".");
+    int = numSplit[0];
+    if (int.length > 3) {
+      int = int.substr(0, int.length - 3) + "," + int.substr(int.lenth - 3, 3);
+    }
+    dec = numSplit[1];
+    return (type === "exp" ? "-" : "+") + " " + int + "." + dec;
+  };
 
   return {
     getInput: function() {
@@ -167,7 +180,7 @@ var UIController = (function() {
       // replace the placeholder text with some actual data
       newHtml = html.replace("%id%", obj.id);
       newHtml = newHtml.replace("%description%", obj.description);
-      newHtml = newHtml.replace("%value%", obj.value);
+      newHtml = newHtml.replace("%value%", formatNumber(obj.value, type));
 
       //insert the HTML into DOM
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
@@ -193,10 +206,18 @@ var UIController = (function() {
     },
 
     displayBudget: function(obj) {
-      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-      document.querySelector(DOMstrings.expenseLabel).textContent =
-        obj.totalExp;
+      obj.budget > 0 ? (type = "inc") : (type = "exp");
+      document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(
+        obj.budget,
+        type
+      );
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(
+        obj.totalInc,
+        "inc"
+      );
+      document.querySelector(
+        DOMstrings.expenseLabel
+      ).textContent = formatNumber(obj.totalExp, "exp");
       document.querySelector(DOMstrings.percentageLabel).textContent =
         obj.percentage;
       if (obj.percentage > 0) {
