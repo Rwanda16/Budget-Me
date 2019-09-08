9edade3acd44d2254dd97ff95e057e0d5ef4e9de
Rwanda16 <ghabiman@gmail.com>
2019-09-07 23:10:37 -0400

    Displayed Current Month and Year

--- app.js
+++ app.js
@@ -140,7 +140,8 @@ var UIController = (function() {
     expenseLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
     container: ".container",
-    expensesPercentageLabel: ".item__percentage"
+    expensesPercentageLabel: ".item__percentage",
+    dateLabel: ".budget__title--month"
   };
   var formatNumber = function(num, type) {
     var numSplit, num, int, dec, type;
@@ -245,6 +246,28 @@ var UIController = (function() {
         }
       });
     },
+    displayMonth: function() {
+      var now, year, month;
+      now = new Date();
+      months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      month = now.getMonth();
+      year = now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent =
+        months[month] + " " + year;
+    },
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -325,6 +348,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log("Application has started.");
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
